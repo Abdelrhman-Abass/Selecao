663ca921f8c2cb7b197a9f248edb1810f88d6e33
Abdelrahman_Abass <92057185+Abdelrhman-Abass@users.noreply.github.com>
2023-11-17 19:56:56 +0200

    add portfolio section with lightbox gallery

--- js/app.js
+++ js/app.js
@@ -7,18 +7,25 @@ const swiperEl = document.querySelector('#testimonials swiper-container')
 const listFilters = document.querySelectorAll(".filters li");
 const imgFilter = document.querySelectorAll('.portfolio-item');
 
+
+
+console.log(window.href)
 window.addEventListener('scroll', () => {
 
     // Change the color of the navbar if the user has scrolled down the page
     header.classList.toggle('scrolled', window.scrollY > 0);
-  
+
+    // Back to top Button
+   
+    
+    
     // Highlight the active navigation link
     sections.forEach(section => {
       const top = window.scrollY;
       const offset = section.offsetTop - 150;
       const height = section.offsetHeight;
       const id = section.getAttribute('id');
-  
+        
       if (top >= offset && top < offset + height) {
         navLinks.forEach(link => {
           link.classList.remove('active');
@@ -51,37 +58,27 @@ toggle.addEventListener('click', () => {
 
 
 
-    listFilters.forEach(li => {
-        li.addEventListener("click", function() {
-            listFilters.forEach(li => li.classList.remove('active-filter'));
-            li.classList.add('active-filter');
 
-            let value = li.textContent.toLowerCase();
-            imgFilter.forEach(div => {
-                let filterValue = div.getAttribute('data-filter');
+listFilters.forEach(li => {
+    li.addEventListener("click", function() {
+        listFilters.forEach(li => li.classList.remove('active-filter'));
+        li.classList.add('active-filter');
+
+        let value = li.textContent.toLowerCase();
+        imgFilter.forEach(div => {
+            let filterValue = div.getAttribute('data-filter');
                 
-                if (filterValue === value || value === "all") {
-                    div.style.display = 'block';
-                } else {
-                    div.style.display = 'none';
-                }
-            });
+            if (filterValue === value || value === "all") {
+                div.style.display = 'block';
+            } else {
+               div.style.display = 'none';
+            }
         });
     });
-
-Object.assign(swiperEl, {
-    slidesPerView: 1,
-    spaceBetween: 10,
-          
-    breakpoints: {
-        640: {
-            slidesPerView: 1,
-            spaceBetween: 20,
-            },
-        1199: {
-            slidesPerView: 3,
-            spaceBetween: 50,
-            },
-    },
 });
+
+
+
+
+
 
